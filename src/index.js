803922edfe5214947b7152451e382ea68b5fde0b
Vignesh <vigneshuvi@gmail.com>
2017-08-17 19:42:46 +0530

    “react-tap-event-plugin” is helps to increase click event fast.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { CookiesProvider } from 'react-cookie';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import createBrowserHistory from 'history/createBrowserHistory'
-
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 
 import App from './containers/App'
@@ -20,6 +20,10 @@ history.listen((location, action) => {
   console.log(`The current URL is ${location.pathname}${location.search}${location.hash}`)
   console.log(`The last navigation action was ${action}`)
 })
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 render(
   <Provider store={store}>
